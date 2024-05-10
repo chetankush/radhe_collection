05db9dbe8e71ca3b8e5e17ff765b20d3d78273e1
chetan kushwah <chetankushwah929@gmail.com>
2024-05-10 15:46:10 +0530

    added routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Link from 'next/link';
+import Header from '../components/Header';  // Assuming Header is exported from this path
+import HomePage from '../components/HomePage';  // Assuming HomePage is exported from this path
+import Footer from '../components/Footer';  // Assuming Footer is exported from this path
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    hello world
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <Header/>
+      <HomePage/>
+      <Footer/>
     </main>
   );
 }
