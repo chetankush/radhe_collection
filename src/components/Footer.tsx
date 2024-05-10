1834ac51a1ff925c493f9f345f7e6a84c4e4429e
chetan kushwah <chetankushwah929@gmail.com>
2024-05-10 16:51:10 +0530

    footer edited

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <div>
             <div className="container mx-auto px-4 py-2 flex flex-col items-center  fixed bottom-0 left-0 right-0">
-                <h1 className="text-xl font-bold">Radhe Collection</h1>
-                <h5>All Rights Reserved</h5>
+                <h1 className="text-lg font-bold">Radhe Collection</h1>
+                <p>All Rights Reserved</p>
                 <p>2024</p>
             </div>
         </div>
