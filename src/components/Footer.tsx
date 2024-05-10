7c3243940a8847f3ed1443107773b4ee8cfbdca9
chetan kushwah <chetankushwah929@gmail.com>
2024-05-10 16:49:08 +0530

    footer chaged

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  )
+    return (
+        <div>
+            <div className="container mx-auto px-4 py-2 flex flex-col items-center  fixed bottom-0 left-0 right-0">
+                <h1 className="text-xl font-bold">Radhe Collection</h1>
+                <h5>All Rights Reserved</h5>
+                <p>2024</p>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
